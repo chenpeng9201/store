3e7787951738c7922036246d734ed968aa131f12
chenp <472126367@qq.com>
2020-07-27 17:25:42 +0800

    测试一下更新

--- src/main/java/com/itheima/dao/UserDao.java
+++ src/main/java/com/itheima/dao/UserDao.java
@@ -5,6 +5,8 @@ import com.itheima.domain.User;
 import java.sql.SQLException;
 
 public interface UserDao {
+    //用户注册
     void register(User user) throws SQLException;
+    //用户登录
     User login(String username,String password) throws SQLException;
 }
