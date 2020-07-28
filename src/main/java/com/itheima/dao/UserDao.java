619a457f114eed3de6f01b8f652106da46ecb853
chenp <472126367@qq.com>
2020-07-28 09:26:06 +0800

    测试提交

--- src/main/java/com/itheima/dao/UserDao.java
+++ src/main/java/com/itheima/dao/UserDao.java
@@ -3,7 +3,10 @@ package com.itheima.dao;
 import com.itheima.domain.User;
 
 import java.sql.SQLException;
-
+/**
+ * author:陈鹏
+ * data:2020.07.28
+ * */
 public interface UserDao {
     //用户注册
     void register(User user) throws SQLException;
